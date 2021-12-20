0d81b7d37870d27dedebe0c9e3cc05c242a8c09e
firetyler <oliver-z-jonsson@hotmail.com>
2021-12-20 14:38:42 +0100

    Grafic pac

---
+++ src/grafik/Grafic.java
@@ -0,0 +1,12 @@
+package grafik;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+public class Grafic extends JFrame{
+
+	public Grafic() {
+		JFileChooser fileChooser = new JFileChooser(currentDirectory);
+	}
+
+}
